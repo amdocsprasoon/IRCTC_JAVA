761c77fe55dd31858dae606bac0e6d073c1eee30
amdocsprasoon <prasoonv@amdocs.com>
2025-04-19 16:12:16 +0530

    Third Commit, Moved the classes to entity Folder

---
+++ entity/IrctcUser.java
@@ -0,0 +1,43 @@
+package entity;
+
+import java.util.List;
+
+public class IrctcUser {
+
+    private Long userId;
+    private String username;
+    private String password;
+    private List<Ticket> bookedTickets;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Ticket> getBookedTickets() {
+        return bookedTickets;
+    }
+
+    public void setBookedTickets(List<Ticket> bookedTickets) {
+        this.bookedTickets = bookedTickets;
+    }
+}
